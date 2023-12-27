b8e8237010764c6269a55dbfdf00428484a2c9eb
Nikita Kolchin <kolchin.nv@gmail.com>
2023-12-27 15:40:30 +0300

    strict mode error

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,78 @@
-import {FC, useEffect, useState} from 'react';
-import LoginForm from "./components/LoginForm";
-import { useAppDispatch, useAppSelector } from './store/hooks';
-import {IUser} from "./models/IUser";
-import UserService from "./services/UserService";
-import axios from 'axios';
-import { checkAuth, logout } from './store/authSlice';
+import { FC, useEffect, useState } from "react"
+import LoginForm from "./components/LoginForm"
+import { useAppDispatch, useAppSelector } from "./store/hooks"
+import { IUser } from "./models/IUser"
+import UserService from "./services/UserService"
+import axios from "axios"
+import { checkAuth, logout } from "./store/authSlice"
 
 const App: FC = () => {
-    const [users, setUsers] = useState<IUser[]>([]);
-    const { isLoading, error, isAuth, user } = useAppSelector(state => state.data);
-    const dispatch = useAppDispatch();
+  const [users, setUsers] = useState<IUser[]>([])
+  const { isLoading, error, isAuth, user } = useAppSelector(
+    (state) => state.data
+  )
+  const dispatch = useAppDispatch()
 
-
-    useEffect(() => {
-
-        const getData = async () => {
-            try {
-                const resp = await axios.get('/api/matches');
-                console.log(resp.data)
-            }
-            catch (e) {
-                console.log(e);
-            }
-            
-        }
-        
-        if (localStorage.getItem('token')) {
-            dispatch(checkAuth())
-        }
-        getData();
-    }, [dispatch])
-
-    async function getUsers() {
-        try {
-            const response = await UserService.fetchUsers();
-            setUsers(response.data);
-        } catch (e) {
-            console.log(e);
-        }
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const resp = await axios.get("/api/matches")
+        console.log(resp.data)
+      } catch (e) {
+        console.log(e)
+      }
     }
 
-    if (isLoading) {
-        return <div>Загрузка...</div>
+    if (localStorage.getItem("token")) {
+      dispatch(checkAuth())
     }
+    getData()
+  }, [dispatch])
 
-    if (error) {
-        return <div>{error}</div>
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers()
+      setUsers(response.data)
+    } catch (e) {
+      console.log(e)
     }
-    if (!isAuth) {
-        return (
-            <div>
-                <LoginForm/>
-                <button onClick={getUsers}>Получить пользователей</button>
-            </div>
-        );
-    }
+  }
 
+  if (isLoading) {
+    return <div>Загрузка...</div>
+  }
+
+  if (error) {
+    console.error("случилось страшное: ", error)
+  }
+  if (!isAuth) {
     return (
-        <div>
-            <h1>{isAuth ? `Пользователь авторизован ${user.email}` : 'АВТОРИЗУЙТЕСЬ'}</h1>
-            <h1>{user.isActivated ? 'Аккаунт подтвержден по почте' : 'ПОДТВЕРДИТЕ АККАУНТ!!!!'}</h1>
-            <button onClick={() => dispatch(logout())}>Выйти</button>
-            <div>
-                <button onClick={getUsers}>Получить пользователей</button>
-            </div>
-            {users.map(user =>
-                <div key={user.email}>{user.email}</div>
-            )}
-        </div>
-    );
-};
+      <div>
+        <LoginForm />
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+    )
+  }
 
-export default App;
+  return (
+    <div>
+      <h1>
+        {isAuth ? `Пользователь авторизован ${user.email}` : "АВТОРИЗУЙТЕСЬ"}
+      </h1>
+      <h1>
+        {user.isActivated
+          ? "Аккаунт подтвержден по почте"
+          : "ПОДТВЕРДИТЕ АККАУНТ!!!!"}
+      </h1>
+      <button onClick={() => dispatch(logout())}>Выйти</button>
+      <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+      {users.map((user) => (
+        <div key={user.email}>{user.email}</div>
+      ))}
+    </div>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,7 @@
-// import {createContext} from 'react';
 import App from './App';
-// import Store from "./store/store";
 import store from './store';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-
-// interface State {
-//     store: Store,
-// }
-
-// export const store = new Store();
-
-// export const Context = createContext<State>({
-//     store,
-// })
 
 const container = document.getElementById('root')
 const root = createRoot(container!); 
