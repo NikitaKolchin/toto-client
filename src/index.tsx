6309133c6ff57c3765c21754c3d38d04f5d96ce2
Nikita Kolchin <kolchin.nv@gmail.com>
2024-01-10 21:51:57 +0300

    added roboto

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import App from './App';
 import store from './store';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
-
-root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-);
+if (container) {
+    const root = createRoot(container);
+    root.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+    );
+}
