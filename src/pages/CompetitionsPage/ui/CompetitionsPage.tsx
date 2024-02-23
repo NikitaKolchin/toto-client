e7dc7b132fbca55f7c94108750cbd42d29d8525e
Nikita Kolchin <kolchin.nv@gmail.com>
2024-02-23 20:36:52 +0300

    added CompetitionEditingTable

--- src/pages/CompetitionsPage/ui/CompetitionsPage.tsx
+++ src/pages/CompetitionsPage/ui/CompetitionsPage.tsx
@@ -1,30 +1,14 @@
-import { useState, useMemo, useDeferredValue } from 'react';
-import { users } from './users';
+import { Container } from '@mui/material';
+import { Title } from 'entities/Title';
+import { CompetitionEditingTable } from 'widgets/CompetitionEditingTable';
 
-function CompetitionsPage() {
-    const [name, setName] = useState('');
-    const listUsers = useMemo(() => {
-        return users.filter((item) => item.name.includes(name));
-    }, [name]);
-    const deferredList = useDeferredValue(listUsers);
-
+const CompetitionsPage = () => {
     return (
-        <div>
-            <div>
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <div>
-                    {deferredList.map((item) => (
-                        <div>{item.name}</div>
-                    ))}
-                </div>
-            </div>
-        </div>
+        <Container>
+            <Title>соревнования</Title>
+            <CompetitionEditingTable />
+        </Container>
     );
-}
+};
+
 export default CompetitionsPage;
---
+++ src/widgets/CompetitionEditingTable/ui/CompetitionEditingTable/CompetitionEditingTable.tsx
@@ -0,0 +1,137 @@
+import { FC, useState } from 'react';
+import { useMemo } from 'react';
+import {
+    MaterialReactTable,
+    useMaterialReactTable,
+    type MRT_ColumnDef,
+    MRT_TableOptions,
+} from 'material-react-table';
+
+import {
+    useGetAllCompetitionsQuery,
+    useUpdateCompetitionByIdMutation,
+    useAddCompetitionMutation,
+} from 'entities/Competition';
+import { getDefaultMRTOptions } from 'shared/DefaultTable';
+import { trueFalse } from 'shared/const/select';
+import type { Competition } from 'shared/api';
+import { Button, Checkbox } from '@mui/material';
+
+const validateRequired = (value: string) => !!value.length;
+
+function validateCompetition(competition: Competition) {
+    return {
+        value: !validateRequired(competition.value) ? 'Name is Required' : '',
+    };
+}
+
+const CompetitionEditingTable: FC = () => {
+    const { data: respondedCompetitions, isLoading } =
+        useGetAllCompetitionsQuery();
+    const [updateCompetition] = useUpdateCompetitionByIdMutation();
+    const [addCompetition] = useAddCompetitionMutation();
+    const [validationErrors, setValidationErrors] = useState<
+        Record<string, string | undefined>
+    >({});
+    const handleSaveCompetition: MRT_TableOptions<Competition>['onEditingRowSave'] =
+        async ({ values, table }) => {
+            const newValidationErrors = validateCompetition(values);
+            if (Object.values(newValidationErrors).some((error) => error)) {
+                setValidationErrors(newValidationErrors);
+                return;
+            }
+            setValidationErrors({});
+            const { email, ...updatedData } = values;
+            await updateCompetition({ ...updatedData });
+            table.setEditingRow(null); //exit editing mode
+        };
+    const handleCreateCompetition: MRT_TableOptions<Competition>['onCreatingRowSave'] =
+        async ({ values, table }) => {
+            const newValidationErrors = validateCompetition(values);
+            if (Object.values(newValidationErrors).some((error) => error)) {
+                setValidationErrors(newValidationErrors);
+                return;
+            }
+            setValidationErrors({});
+            const { id, ...updatedData } = values;
+            await addCompetition(updatedData);
+            table.setCreatingRow(null); //exit creating mode
+        };
+    const competitions: Competition[] = respondedCompetitions || [];
+    const columns = useMemo<MRT_ColumnDef<Competition>[]>(
+        () => [
+            {
+                accessorKey: 'id',
+                header: 'id',
+                enableEditing: false,
+            },
+            {
+                accessorKey: 'value',
+                header: 'value',
+                muiEditTextFieldProps: {
+                    required: true,
+                    error: !!validationErrors?.value,
+                    helperText: validationErrors?.value,
+                    onFocus: () =>
+                        setValidationErrors({
+                            ...validationErrors,
+                            value: undefined,
+                        }),
+                },
+            },
+            {
+                accessorKey: 'isCurrent',
+                header: 'isCurrent',
+                type: 'boolean',
+                editSelectOptions: trueFalse,
+                editVariant: 'select',
+                Cell: ({ row }) => (
+                    <Checkbox
+                        key={`isCurrent${row.id}`}
+                        disabled
+                        checked={row.original.isCurrent}
+                    />
+                ),
+            },
+        ],
+        [validationErrors],
+    );
+    const defaultMRTOptions = getDefaultMRTOptions<Competition>();
+    const table = useMaterialReactTable({
+        ...defaultMRTOptions,
+        columns,
+        data: competitions,
+        state: {
+            isLoading,
+        },
+        getRowId: (row) => row.id,
+        onEditingRowSave: handleSaveCompetition,
+        onEditingRowCancel: () => setValidationErrors({}),
+        onCreatingRowCancel: () => setValidationErrors({}),
+        onCreatingRowSave: handleCreateCompetition,
+        renderTopToolbarCustomActions: ({ table }) => (
+            <Button
+                variant="contained"
+                onClick={() => {
+                    table.setCreatingRow(true); //simplest way to open the create row modal with no default values
+                    //or you can pass in a row object to set default values with the `createRow` helper function
+                    // table.setCreatingRow(
+                    //   createRow(table, {
+                    //     //optionally pass in default values for the new row, useful for nested data or other complex scenarios
+                    //   }),
+                    // );
+                }}
+            >
+                Create New Competition
+            </Button>
+        ),
+        initialState: {
+            columnVisibility: {
+                id: false,
+            },
+        },
+    });
+    return <MaterialReactTable table={table} />;
+};
+
+export { CompetitionEditingTable };
