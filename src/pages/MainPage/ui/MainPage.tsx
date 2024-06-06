fb883806a701685346127a8d7a90ca4bf1207a7f
Nikita Kolchin <kolchin.nv@gmail.com>
2024-06-06 16:06:29 +0300

    activation and allow only for auth users

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -62,48 +62,58 @@ const Main: FC = () => {
                         </CardActions>
                     </Card>
                 </Grid>
-                <Grid item xs={12} sm={4} md={4}>
-                    <Card>
-                        <CardHeader title="Активация" />
-                        <CardContent>
-                            {' '}
-                            {user.isActivated
-                                ? `Аккаунт подтвержден по почте ${user.email}`
-                                : 'Необходимо подтвердить почту'}
-                        </CardContent>
-                        <CardActions>
-                            {user.isActivated ? (
-                                ''
-                            ) : (
-                                <Button
-                                    size="small"
-                                    onClick={() => navigate(AppRoutes.PROFILE)}
-                                >
-                                    Активировать
-                                </Button>
-                            )}
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={4} md={4}>
-                    <Card>
-                        <CardHeader title="Допуск" />
-                        <CardContent>
-                            {user.isAllowed
-                                ? `Допуск получен, ${user.firstName}`
-                                : 'Необходимо получить допуск'}
-                        </CardContent>
-                        <CardActions>
-                            {user.isAllowed ? (
-                                ''
-                            ) : (
-                                <Button size="small" onClick={handleAllowQuery}>
-                                    Обратиться за допуском
-                                </Button>
-                            )}
-                        </CardActions>
-                    </Card>
-                </Grid>
+                {isAuth && (
+                    <>
+                        {' '}
+                        <Grid item xs={12} sm={4} md={4}>
+                            <Card>
+                                <CardHeader title="Активация" />
+                                <CardContent>
+                                    {' '}
+                                    {user.isActivated
+                                        ? `Аккаунт подтвержден по почте ${user.email}`
+                                        : 'Необходимо подтвердить почту'}
+                                </CardContent>
+                                <CardActions>
+                                    {user.isActivated ? (
+                                        ''
+                                    ) : (
+                                        <Button
+                                            size="small"
+                                            onClick={() =>
+                                                navigate(AppRoutes.PROFILE)
+                                            }
+                                        >
+                                            Активировать
+                                        </Button>
+                                    )}
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} sm={4} md={4}>
+                            <Card>
+                                <CardHeader title="Допуск" />
+                                <CardContent>
+                                    {user.isAllowed
+                                        ? `Допуск получен, ${user.firstName}`
+                                        : 'Необходимо получить допуск'}
+                                </CardContent>
+                                <CardActions>
+                                    {user.isAllowed ? (
+                                        ''
+                                    ) : (
+                                        <Button
+                                            size="small"
+                                            onClick={handleAllowQuery}
+                                        >
+                                            Обратиться за допуском
+                                        </Button>
+                                    )}
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    </>
+                )}
             </Grid>
         </Container>
     );
